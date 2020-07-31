11ac8e65c1a993f68cad79026c98b1c814bdae21
shunici <32427612+shunici@users.noreply.github.com>
2020-07-31 22:27:36 +0700

    pemahaman arrow function

---
+++ objek/objek2/arrow_function.js
@@ -0,0 +1,47 @@
+//harap matikan komentar jika ingin menggunakan
+
+//fungsi biasa
+// var halo = function(nama){
+//     return `halo ${nama}`;
+// }
+// console.log( halo('shunii') );
+
+
+
+
+//arrow fungsi adalah bentuk fungsi yang lebih ringkas cth
+// let halo = (nama) => {
+//     return `halo ${nama}`;
+// }
+// console.log( halo('shunii') );
+
+
+
+
+
+//jika parameter lebih dari satu
+// let halo = (nama, waktu) => {
+//     return `halo ${nama} selamat ${waktu}`;
+// }
+// console.log( halo('shunii', 'pagi') );
+
+
+//jika parameter satu aja
+// let halo = nama => {
+//     return `halo ${nama} selamat`;
+// }
+// console.log( halo('shunii') );
+
+
+
+
+
+//jika return nya cuma satu aja (implisit return)
+// let halo = nama => `halo ${nama} selamat`;
+// console.log( halo('shunii') );
+
+
+//jika fungsinya tanpa parameter
+let halo = () => `hy shunici`;
+console.log( halo() );
+
