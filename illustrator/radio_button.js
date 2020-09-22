eba7ad479f34f67dd739969a4769d6b6444ab09a
shunici <32427612+shunici@users.noreply.github.com>
2020-09-22 23:35:08 +0700

    Update radio_button.js

--- illustrator/radio_button.js
+++ illustrator/radio_button.js
@@ -15,3 +15,34 @@ radio2.onClick = function (){
     }
 
 w.show();
+
+
+/// versi kedua
+
+var w = new Window ("dialog");
+w.alignChildren = "left";
+var myButtonGroup = w.add ("group");
+myButtonGroup.orientation = "column";
+myButtonGroup.alignment = "left";
+var radio1 = myButtonGroup.add ("radiobutton", undefined, "Radio Button 1");
+var radio2 = myButtonGroup.add ("radiobutton", undefined, "Radio Button 2");
+myButtonGroup.add ("button", undefined, "OK");
+myButtonGroup.add ("button", undefined, "Cancel");
+radio1.value = true;
+w.center(); 
+// w.show ();
+
+if (w.show() == 1)
+{
+    alert("You picked " + youSelected(myButtonGroup))
+}
+
+function youSelected(rButtons)
+{
+    if (radio1.value == true)
+    // radio1 selected
+    return radio1.text
+    else
+    // radio2 selected
+    return radio2.text
+}
