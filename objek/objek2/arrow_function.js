86db5fe3eede9a39bd4b929ec4a16f4b743b4707
shunici <32427612+shunici@users.noreply.github.com>
2020-11-17 22:44:01 +0700

    arrow function

--- objek/objek2/arrow_function.js
+++ objek/objek2/arrow_function.js
@@ -1,47 +1,52 @@
-//harap matikan komentar jika ingin menggunakan
+
+function halo (nama){
+ return alert(nama);
+}
+halo('shunii');
+
+// ini sama yang diatas
 
 //fungsi biasa
-// var halo = function(nama){
-//     return `halo ${nama}`;
-// }
-// console.log( halo('shunii') );
+var halo = function(nama){
+    return `halo ${nama}`;
+}
+console.log( halo('shunii') );
 
 
 
 
 //arrow fungsi adalah bentuk fungsi yang lebih ringkas cth
-// let halo = (nama) => {
-//     return `halo ${nama}`;
-// }
-// console.log( halo('shunii') );
+let halo = (nama) => {
+    return `halo ${nama}`;
+}
+console.log( halo('shunii') );
 
 
 
 
 
 //jika parameter lebih dari satu
-// let halo = (nama, waktu) => {
-//     return `halo ${nama} selamat ${waktu}`;
-// }
-// console.log( halo('shunii', 'pagi') );
+let halo = (nama, waktu) => {
+    return `halo ${nama} selamat ${waktu}`;
+}
+console.log( halo('shunii', 'pagi') );
 
 
 //jika parameter satu aja
-// let halo = nama => {
-//     return `halo ${nama} selamat`;
-// }
-// console.log( halo('shunii') );
+let halo = nama => {
+    return `halo ${nama} selamat`;
+}
+console.log( halo('shunii') );
 
 
 
 
 
 //jika return nya cuma satu aja (implisit return)
-// let halo = nama => `halo ${nama} selamat`;
-// console.log( halo('shunii') );
+let halo = nama => `halo ${nama} selamat`;
+console.log( halo('shunii') );
 
 
 //jika fungsinya tanpa parameter
 let halo = () => `hy shunici`;
 console.log( halo() );
-
